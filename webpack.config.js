e4da1dad615c89b8aab315d21de5de57b740323c
Michael Weber <webermn15@gmail.com>
2018-04-10 15:48:57 -0400

    set up webpack config for react development with eslint

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+let path = require('path');
+
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, './dist/public'),
+    publicPath: '/'
+	},
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist/public')
+  },
+  mode: 'development'
+}
